39e3d641a6830744b13401878fa2bb716866f81d
Karina Nguyen <143886454+KarinaeNguyen@users.noreply.github.com>
2025-11-15 13:02:43 +0700

    Update main_v11.js

--- assets/js/main_v11.js
+++ assets/js/main_v11.js
@@ -1,5 +1,8 @@
+// This is the COMPLETE file for assets/js/main_v11.js
 (() => {
   window.initializeApp = function initializeApp() {
+    console.log('initializeApp() called. Starting modules...');
+    
     if (typeof window.initTabs === 'function') {
       window.initTabs();
     }
